230a4a68ee3e12bd11d9af3a9c089c9b8fc2023c
Lynch Mwaniki <lsking16@gmail.com>
2019-04-12 00:55:35 +0200

    Modified MrJob Inverted Index to read file name from command line.

--- MrJob/inverted index/runInvertedIndexing.py
+++ MrJob/inverted index/runInvertedIndexing.py
@@ -1,11 +1,12 @@
 from invertedIndexing import MRInvertedIndex
+import sys
 
 word_line_numbers_pairs = []
-mr_job = MRInvertedIndex(args=['mediumText.txt'])
+mr_job = MRInvertedIndex(args=sys.argv[1:])
 with mr_job.make_runner() as runner:
 	runner.run()
 	for word, line_numbers in mr_job.parse_output(runner.cat_output()):
 		word_line_numbers_pairs.append([word, line_numbers])
 	
 	for distinct_word in word_line_numbers_pairs[:50]:
-		print(distinct_word)#Limit output to 50 as stated in lab brief.
+		print(distinct_word[0], " ", distinct_word[1])#Limit output to 50 as stated in lab brief.
